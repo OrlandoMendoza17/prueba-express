f93cfb4f29a617a113c9f9b2f97916f61412891b
OrlandoMendoza17 <ommv.17@gmail.com>
2021-12-10 02:04:42 -0400

    La lógica de la aplicación ahora está en index.js

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
-require("@babel/register")({
-  presets: [
-    "@babel/preset-env",
-  ]
+const express = require("express")
+
+const port = 3000;
+
+const app = express();
+
+const router = express.Router()
+
+router.get("*", (request, response) => {
+  response.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+      
+      <style>
+      
+        body{
+          
+          font-family: "arial";
+          
+        }
+        
+        article{
+          padding: 10px;
+          background-color: cyan;
+          font-weight: 700;
+        }
+      
+      </style>
+      
+      </head>
+      <body>
+        <article>
+          Hola doctor, estás en la dirección: "${request.url}"
+        </article>  
+      </body>
+    
+    </html>
+  `)
 })
 
-require("./src/server")
+app.use("/", router)
+
+app.listen(port, (error) => {
+
+  if (error) 
+    console.log("Ha habido un error mi negro :'(")
+  else 
+    console.log(`The server is running on ${port} port`)
+
+})
